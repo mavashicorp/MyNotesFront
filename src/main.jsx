e8250370fa60c4ef19f356bf3fbce09bdc7b4033
Dmitriy <mavashicorp@gmail.com>
2024-05-24 21:00:12 +0300

    add ChakraProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
+// import * as React from 'react'
+// import React from 'react'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ChakraProvider>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>,
 )
